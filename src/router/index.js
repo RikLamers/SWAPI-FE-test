e46aa425e0f56953985da0f1606e5ac49fe76438
Rik Lamers <info@riklamers.com>
2020-01-06 14:26:10 +0100

    Initial Vue commit
    Added all basic files

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import Home from '../views/Home.vue'
+import Planets from '@/views/Planets.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/planets',
+    name: 'planets',
+    component: Planets
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router

0ebd9387528852f7b08b13ec17da78c6c76bc537
Rik Lamers <info@riklamers.com>
2020-01-07 15:42:37 +0100

    Import bootstrap grid + import scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// CSS
+import './assets/css/reset.css'
+import './assets/css/bootstrap-grid.min.css'
+
+// Custom SCSS
+import './assets/scss/index.scss'
+
 Vue.config.productionTip = false
 
 new Vue({

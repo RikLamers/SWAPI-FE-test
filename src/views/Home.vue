43b2d339cb1653101d1238a4e58cd32c6b930972
Rik Lamers <info@riklamers.com>
2020-01-08 14:17:40 +0100

    Updated home view with all HTML elements + SCSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,95 @@
 <template>
   <div>
+    <Header />
+    <main class="container">
+      <div class="v-home">
+        <section class="characters">
+          <div class="row">
+            <div class="col-12">
+              <h2>Populair Starships</h2>
+              <hr />
+            </div>
+          </div>
+          <div class="row">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <h1>pagination</h1>
+            </div>
+          </div>
+        </section>
+        <section class="planets">
+          <div class="row">
+            <div class="col-12">
+              <h2>Populair Planets</h2>
+              <hr />
+            </div>
+          </div>
+          <div class="row">
+            <Card type="img-only" />
+            <Card type="img-only" />
+            <Card type="img-only" />
+          </div>
+          <div class="row">
+            <div class="col-12">
+              <ul>
+                <li></li>
+                <li class="is--active"></li>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+        </section>
+        <section class="characters">
+          <div class="row">
+            <div class="col-12">
+              <h2>Populair Characters</h2>
+              <hr />
+            </div>
+          </div>
+          <div class="row">
+            <Card type="row" />
+            <Card type="row" />
+            <Card type="row" />
+            <Card type="row" />
+          </div>
+          <div class="row">
+            <h1>pagination</h1>
+          </div>
+        </section>
+      </div>
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
+import Card from '@/components/Card.vue'
+import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue'
+
 export default {
-  name: 'home'
+  name: 'home',
+  components: {
+    Card,
+    Footer,
+    Header
+  },
+  created () {
+    this.$store.dispatch('getAllData')
+  },
+  computed: {
+    ...mapState([
+      'characters'
+    ])
+  }
 }
 </script>

55de647bb976dc5a3b1dbdce887aff345ac20d4e
Rik Lamers <info@riklamers.com>
2020-01-08 14:16:49 +0100

    Added SearchBar component + SCSS

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="c-searchbar">
+    <button class="c-searchbar__btn">
+      <img src="~@/assets/img/icons/search.svg" alt="Search button icon" />
+    </button>
+    <input class="c-searchbar__input" type="search" placeholder="Enter a search term" />
+  </div>
+</template>

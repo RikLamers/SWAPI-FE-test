5e7d56af811c3d489dac309277e14619fee8a17a
Rik Lamers <info@riklamers.com>
2020-01-09 23:28:49 +0100

    Added Title component

---
+++ src/components/Title.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="c-title" :style="margin ? margin : null">
+  <div class="row">
+    <div class="col-12">
+      <h2>{{ title }}</h2>
+      <hr />
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: [
+    'title',
+    'margin'
+  ],
+  mounted () {
+    console.log(this.margin)
+  }
+}
+</script>

42bfb48f265c3760df8ed2d20783f8ae6bb2470b
Rik Lamers <info@riklamers.com>
2020-01-09 23:29:53 +0100

    Updated Pagination component
    Added new variant

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="c-pagination">
+  <!-- FIXME: right method? -->
+  <div v-if="type === 'arrow'" :class="type === 'arrow' ? 'c-pagination--arrow' : null">
+    <span>1-8 of 50</span>
+    <button>
+      <Arrow :isDisabled=leftDisabled rotateDeg="180" height="16px" width="16px" />
+    </button>
+    <button>
+      <Arrow :isDisabled=rightDisabled height="16px" width="16px" />
+    </button>
+  </div>
+  <div v-else class="c-pagination">
     <button>View more</button>
   </div>
 </template>
 
 <script>
+import Arrow from '@/components/icons/Arrow.vue'
+
 export default {
-  name: 'pagination'
+  props: [
+    'type'
+  ],
+  data () {
+    return {
+      leftDisabled: true,
+      rightDisabled: false
+    }
+  },
+  components: {
+    Arrow
+  }
 }
 </script>

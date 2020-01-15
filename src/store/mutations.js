c959b9312592cae6d17e185edaafb831889e195e
Rik Lamers <info@riklamers.com>
2020-01-15 16:23:49 +0100

    Added mutations for api calls

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,5 +1,25 @@
 export default {
-  getInitData (state, payload) {
+  getCharacterData (state, payload) {
     state.characters = payload
+  },
+
+  addAllCharacters (state, payload) {
+    state.characters.results = [...state.characters.results, ...payload]
+  },
+
+  getPlanetData (state, payload) {
+    state.planets = payload
+  },
+
+  addAllPlanets (state, payload) {
+    state.planets.results = [...state.planets.results, ...payload]
+  },
+
+  getStarshipData (state, payload) {
+    state.starships = payload
+  },
+
+  addAllStarships (state, payload) {
+    state.starships.results = [...state.starships.results, ...payload]
   }
 }

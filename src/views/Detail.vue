510c9e9d7fd8e9ccafa96a5542d93852d8dd04bb
Rik Lamers <info@riklamers.com>
2020-01-15 16:23:04 +0100

    Added detail component, loaded in all data

---
+++ src/views/Detail.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <Header />
+    <main class="container">
+      <div class="v-detail">
+        <section class="detail">
+          <Title :title="detailedData.name" :margin="titleMargin" />
+          <div class="v-detail__content">
+            <div class="row">
+              <div class="col-12 col-md-6">
+                <div class="v-detail__center">
+                  <img src="~@/assets/img/planets/planet-2.jpg" alt="Planeetje" />
+                </div>
+              </div>
+              <div class="col-12 col-md-6">
+                <div class="v-detail__center">
+                  <Table :data="detailedData" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer.vue'
+import Header from '@/components/Header.vue'
+import Table from '@/components/Table.vue'
+import Title from '@/components/Title.vue'
+
+export default {
+  components: {
+    Footer,
+    Header,
+    Table,
+    Title
+  },
+  data () {
+    return {
+      titleMargin: 'margin-bottom: 50px',
+      detailedData: {}
+    }
+  },
+  created () {
+    // split the path at /
+    const splitLink = this.$route.path.split('/')
+    // get the array to search
+    const arrayToSearch = splitLink[1]
+    // get the id to search in the array
+    const id = this.$route.params.id
+    // get the data based on the id
+    this.detailedData = this.$store.getters.getDetails(arrayToSearch, id)[0]
+  }
+}
+</script>

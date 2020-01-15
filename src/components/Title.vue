d07476be22e76113c49484014363dabd60a34066
Rik Lamers <info@riklamers.com>
2020-01-15 16:25:07 +0100

    Updated title component to accept links

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -2,7 +2,10 @@
 <div class="c-title" :style="margin ? margin : null">
   <div class="row">
     <div class="col-12">
-      <h2>{{ title }}</h2>
+      <router-link v-if="link" :to="link">
+        <h2>{{ title }}</h2>
+      </router-link>
+      <h2 v-else>{{ title }}</h2>
       <hr />
     </div>
   </div>
@@ -12,11 +15,9 @@
 <script>
 export default {
   props: [
+    'link',
     'title',
     'margin'
-  ],
-  mounted () {
-    console.log(this.margin)
-  }
+  ]
 }
 </script>

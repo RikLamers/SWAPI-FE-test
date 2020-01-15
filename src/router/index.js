d85e063b80cb7e7750ef79890ac67aa6bb0f6ebf
Rik Lamers <info@riklamers.com>
2020-01-15 16:24:53 +0100

    Imported all components and added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-import Home from '../views/Home.vue'
 import Characters from '@/views/Characters.vue'
+import Detail from '@/views/Detail.vue'
+import Home from '@/views/Home.vue'
+import Planets from '@/views/Planets.vue'
 import Starships from '@/views/Starships.vue'
 
 Vue.use(VueRouter)
@@ -19,9 +21,29 @@ const routes = [
     component: Characters
   },
   {
+    path: '/characters/:id',
+    name: 'character detail',
+    component: Detail
+  },
+  {
+    path: '/planets',
+    name: 'planets',
+    component: Planets
+  },
+  {
+    path: '/planets/:id',
+    name: 'planet detail',
+    component: Detail
+  },
+  {
     path: '/starships',
     name: 'starships',
     component: Starships
+  },
+  {
+    path: '/starships/:id',
+    name: 'starship detail',
+    component: Detail
   }
 ]
 

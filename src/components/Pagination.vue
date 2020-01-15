97f6c90a0af36c4dcdd5226bd4446d3c46c60a12
Rik Lamers <info@riklamers.com>
2020-01-15 16:27:09 +0100

    Updated pagination to accept various props

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,16 +1,12 @@
 <template>
-  <!-- FIXME: right method? -->
-  <div v-if="type === 'arrow'" :class="type === 'arrow' ? 'c-pagination--arrow' : null">
-    <span>1-8 of 50</span>
-    <button>
-      <Arrow :isDisabled=leftDisabled rotateDeg="180" height="16px" width="16px" />
+  <div class="c-pagination">
+    <span>{{ itemNumbMin }}-{{ itemNumbMax }} of {{ paginationCount }}</span>
+    <button :disabled="!previousAvail" @click="getNewData('previous')">
+      <Arrow :isDisabled="!previousAvail" rotateDeg="180" height="16px" width="16px" />
     </button>
-    <button>
-      <Arrow :isDisabled=rightDisabled height="16px" width="16px" />
+    <button :disabled="!nextAvail" @click="getNewData('next')">
+      <Arrow :isDisabled="!nextAvail" height="16px" width="16px" />
     </button>
-  </div>
-  <div v-else class="c-pagination">
-    <button>View more</button>
   </div>
 </template>
 
@@ -19,16 +15,20 @@ import Arrow from '@/components/icons/Arrow.vue'
 
 export default {
   props: [
-    'type'
+    'type',
+    'paginationCount',
+    'itemNumbMin',
+    'itemNumbMax',
+    'previousAvail',
+    'nextAvail'
   ],
-  data () {
-    return {
-      leftDisabled: true,
-      rightDisabled: false
-    }
-  },
   components: {
     Arrow
+  },
+  methods: {
+    getNewData (nextOrPrevious) {
+      this.$emit('requestNewData', nextOrPrevious)
+    }
   }
 }
 </script>

ccbee2714a550b3d2913a213c16c8a468aff28b5
Rik Lamers <info@riklamers.com>
2020-01-15 16:27:37 +0100

    Updated card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,32 +1,39 @@
 <template>
-  <div :class="type === 'row' ? 'col-12 col-lg-6' : 'col-12 col-md-6 col-lg-4'">
-    <div v-if="type === 'img-only'" class="c-card" :class="type === 'img-only' ? 'c-card--img-only' : null">
+  <div v-if="type === 'img-only'" class="c-card" :class="type === 'img-only' ? 'c-card--img-only' : null">
+    <router-link :to="`${data.category}/${data.id}`">
       <img src="~@/assets/img/planets/planet-1.jpg" alt="Planet 1" />
       <div class="c-card__footer">
-        <h3>Name planet</h3>
+        <h3>{{ data.name }}</h3>
+        <p>Population: {{ data.population }}</p>
+        <p>Climate: {{ data.climate }}</p>
       </div>
-    </div>
-    <div v-else-if="type === 'row'" class="c-card" :class="type === 'row' ? 'c-card--row' : null">
+    </router-link>
+  </div>
+  <div v-else-if="type === 'row'" class="c-card" :class="type === 'row' ? 'c-card--row' : null">
+    <router-link :to="`${data.category}/${data.id}`">
       <img class="c-card__img" src="~@/assets/img/characters/character-1.jpg" alt="Star Wars character" />
       <div class="c-card__content">
-        <h3>Dit is een kat</h3>
-        <i>Son of Anakin</i>
+        <h3>{{ data.name }}</h3>
+        <i>Gender: {{ data.gender }}</i>
         <div class="c-card__content__p">
-          <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
-          <span>
+          <p>Birth year: {{ data.birth_year }}</p>
+          <!-- <span>
             <a href="#">read More</a>
-          </span>
+          </span> -->
         </div>
       </div>
-    </div>
-    <div v-else class="c-card">
-      <div class="c-card__img"></div>
-      <div class="c-card__content__bg">
-        <div class="c-card__content">
-          <h3>Dit is een kat</h3>
-          <p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
-          <button>Read more <span>></span></button>
-        </div>
+    </router-link>
+  </div>
+  <div v-else class="c-card">
+    <img class="c-card__img" src="~@/assets/img/starships/starship-1.jpg" alt="Star Wars Starships" />
+    <div class="c-card__content__bg">
+      <div class="c-card__content">
+        <h3>{{ data.name }}</h3>
+        <p>Model: {{ data.model }}</p>
+        <p>passengers: {{ data.passengers }}</p>
+        <router-link :to="`${data.category}/${data.id}`">
+          Read more <span>></span>
+        </router-link>
       </div>
     </div>
   </div>
@@ -35,7 +42,9 @@
 <script>
 export default {
   props: [
-    'type'
+    'type',
+    'data',
+    'link'
   ]
 }
 </script>

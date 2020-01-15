64cf126f94764f5c9abf9c5be44efbdbb4b67f19
Rik Lamers <info@riklamers.com>
2020-01-15 16:27:24 +0100

    Added loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="lds-ring">
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+        <div>
+        </div>
+    </div>
+</template>

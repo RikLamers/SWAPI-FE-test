91c4a9a126b0fb150d19daeb6b75c739a3e1e1c1
Rik Lamers <info@riklamers.com>
2020-01-16 03:45:42 +0100

    Added ability to search through all data

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,25 +1,31 @@
 export default {
   getCharacterData (state, payload) {
     state.characters = payload
+    state.allData = [...state.allData, ...payload.results]
   },
 
   addAllCharacters (state, payload) {
     state.characters.results = [...state.characters.results, ...payload]
+    state.allData = [...state.allData, ...payload]
   },
 
   getPlanetData (state, payload) {
     state.planets = payload
+    state.allData = [...state.allData, ...payload.results]
   },
 
   addAllPlanets (state, payload) {
     state.planets.results = [...state.planets.results, ...payload]
+    state.allData = [...state.allData, ...payload]
   },
 
   getStarshipData (state, payload) {
     state.starships = payload
+    state.allData = [...state.allData, ...payload.results]
   },
 
   addAllStarships (state, payload) {
     state.starships.results = [...state.starships.results, ...payload]
+    state.allData = [...state.allData, ...payload]
   }
 }
--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,6 @@
 export default {
   characters: [],
   planets: [],
-  starships: []
+  starships: [],
+  allData: []
 }

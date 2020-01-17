93dcb20b060cdc1e1191daa1dc001f4454135e77
Rik Lamers <info@riklamers.com>
2020-01-17 16:17:44 +0100

    Updated Arrow icon for two types of arrows

--- src/components/icons/Arrow.vue
+++ src/components/icons/Arrow.vue
@@ -1,7 +1,12 @@
 <template>
-  <svg x="0px" y="0px" viewBox="0 0 6.8 12" :height="height" :width="width">
+  <svg v-if="type === 'small'" x="0px" y="0px" viewBox="0 0 6.8 12" :height="height" :width="width">
     <g :fill="isDisabled ? '#A8A8A8' : fill" :transform="`rotate(${rotateDeg})`" >
       <path d="M6.6,6.6l-5.2,5.2c-0.3,0.3-0.9,0.3-1.2,0c-0.3-0.3-0.3-0.9,0-1.2L4.8,6L0.2,1.4c-0.3-0.3-0.3-0.9,0-1.2 c0.3-0.3,0.9-0.3,1.2,0l5.2,5.2C6.8,5.6,6.8,5.8,6.8,6C6.8,6.2,6.8,6.4,6.6,6.6z"/>
+    </g>
+  </svg>
+  <svg v-else x="0px" y="0px" viewBox="0 0 12 8.4" :height="height" :width="width">
+    <g :fill="isDisabled ? '#A8A8A8' : fill" :transform="`rotate(${rotateDeg})`" >
+      <path d="M8.1,1.9c-0.2-0.2-0.4-0.2-0.6,0c-0.2,0.2-0.2,0.4,0,0.6l3.1,3.1H0.4C0.2,5.6,0,5.8,0,6c0,0.2,0.2,0.4,0.4,0.4 h10.1L7.5,9.5c-0.2,0.2-0.2,0.4,0,0.6c0.2,0.2,0.4,0.2,0.6,0l3.8-3.8c0.2-0.2,0.2-0.4,0-0.6L8.1,1.9z"/>
     </g>
   </svg>
 </template>
@@ -9,8 +14,12 @@
 <script>
 export default {
   props: {
+    type: {
+      type: String,
+      default: ''
+    },
     fill: {
-      type: [String],
+      type: String,
       default: '#505050'
     },
     rotateDeg: {

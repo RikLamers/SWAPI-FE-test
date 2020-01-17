20a83c433e995fcb2130c077abe69b9ca59d0ac6
Rik Lamers <info@riklamers.com>
2020-01-17 03:52:04 +0100

    Fixed typo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -25,7 +25,7 @@
     <div class="row">
       <div class="col-12 col-md-6 offset-md-3">
         <div class="c-header__subtitle">
-          <h5>Find your favorite Star Wars Characters, Films, species, Starships ans Planets!</h5>
+          <h5>Find your favorite Star Wars Characters, Films, species, Starships and Planets!</h5>
         </div>
       </div>
     </div>

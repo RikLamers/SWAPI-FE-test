5dec90cc338f1cc0ebbbeccd58cb27c872dd2130
Rik Lamers <info@riklamers.com>
2020-01-17 03:51:54 +0100

    Added validation on table data output
    What data to display and what should be left in the object

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,14 +1,11 @@
 <template>
   <div class="c-table">
-    <div class="c-table__header">
-
-    </div>
     <div class="c-table__body">
       <div class="c-table__row" v-for="(value, key, index) of data" :key="index">
-        <div class="c-table__column" v-if="typeof value !== 'object'">
+        <div class="c-table__column" v-if="validateData(key, value)">
           {{ key | removeUnderscore | capatalize }}:
         </div>
-        <div class="c-table__column" v-if="typeof value !== 'object'">
+        <div class="c-table__column" v-if="validateData(key, value)">
           {{ value }}
         </div>
       </div>
@@ -23,13 +20,17 @@ export default {
   ],
   filters: {
     removeUnderscore (value) {
-      if (value.includes('_')) {
-        return value.split('_').join(' ')
-      }
+      if (value.includes('_')) return value.split('_').join(' ')
       return value
     },
     capatalize (value) {
       return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  methods: {
+    validateData (key, value) {
+      if (typeof value !== 'object' && key !== 'homeworld' && key !== 'id' && key !== 'category' && key !== 'url' && key !== 'created' && key !== 'edited' && key !== 'img') return true
+      return false
     }
   }
 }

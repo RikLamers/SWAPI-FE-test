eb3e16cb7e5e172e003958dd204f2e462ccf15f9
Rik Lamers <info@riklamers.com>
2020-01-20 21:19:01 +0100

    Added Darth Vader img to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
     <footer class="c-footer">
-
+        <img src="@/assets/img/transparant-vader.png" alt="Darth Vader - Luke I'm your.. FATHER - Star Wars" />
     </footer>
 </template>
